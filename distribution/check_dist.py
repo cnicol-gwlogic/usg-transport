36ed1d2c61f0330eeff425580c1bf2b131921b18
Chris Nicol <chris.nicol@groundwaterlogic.com>
2023-08-06 14:02:17 +1000

    fix(check_dist.py)

--- distribution/check_dist.py
+++ distribution/check_dist.py
@@ -20,17 +20,11 @@ _fc = environ.get("FC", None)
 _included_dir_paths = {
     "full": [
         "bin",
-        "doc",
-        "examples",
         "src",
-        "srcbmi",
-        "msvs",
         "make",
-        "utils",
     ],
     "minimal": [
         "bin",
-        "doc",
     ],
 }
 
@@ -76,20 +70,7 @@ def test_sources(dist_dir_path, releasemode, full):
         pytest.skip(reason="sources not included in minimal distribution")
 
     assert (dist_dir_path / "src").is_dir()
-    assert (dist_dir_path / "src" / "mf6.f90").is_file()
-
-    version_file_path = dist_dir_path / "src" / "Utilities" / "version.f90"
-    assert version_file_path.is_file()
-
-    # find IDEVELOPMODE line
-    lines = open(version_file_path, "r").read().splitlines()
-    pattern = ":: IDEVELOPMODE ="
-    line = next(iter([l for l in lines if pattern in l]), None)
-    assert line
-
-    # make sure IDEVELOPMODE was set correctly
-    idevelopmode = 0 if releasemode else 1
-    assert f"IDEVELOPMODE = {idevelopmode}" in line
+    assert (dist_dir_path / "src" / "mfusg.f").is_file()
 
 
 @pytest.mark.skipif(not _fc, reason="needs Fortran compiler")
@@ -99,60 +80,13 @@ def test_makefiles(dist_dir_path, full):
 
     assert (dist_dir_path / "make" / "makefile").is_file()
     assert (dist_dir_path / "make" / "makedefaults").is_file()
-    assert (dist_dir_path / "utils" / "zonebudget" / "make" / "makefile").is_file()
-    assert (dist_dir_path / "utils" / "zonebudget" / "make" / "makedefaults").is_file()
-    assert (dist_dir_path / "utils" / "mf5to6" / "make" / "makefile").is_file()
-    assert (dist_dir_path / "utils" / "mf5to6" / "make" / "makedefaults").is_file()
 
     # makefiles can't be used on Windows with ifort compiler
     if _system != "Windows" or _fc != "ifort":
         print(subprocess.check_output("make", cwd=dist_dir_path / "make", shell=True))
-        print(
-            subprocess.check_output(
-                "make", cwd=dist_dir_path / "utils" / "zonebudget" / "make", shell=True
-            )
-        )
-        print(
-            subprocess.check_output(
-                "make", cwd=dist_dir_path / "utils" / "mf5to6" / "make", shell=True
-            )
-        )
+        
 
-
-def test_msvs(dist_dir_path, full):
-    if not full:
-        pytest.skip(reason="MSVS files not included in minimal distribution")
-
-    assert (dist_dir_path / "msvs" / "mf6.sln").is_file()
-    assert (dist_dir_path / "msvs" / "mf6.vfproj").is_file()
-    assert (dist_dir_path / "msvs" / "mf6bmi.sln").is_file()
-    assert (dist_dir_path / "msvs" / "mf6bmi.vfproj").is_file()
-    assert (dist_dir_path / "msvs" / "mf6core.vfproj").is_file()
-
-
-def test_docs(dist_dir_path, full):
-    # mf6io should always be included
-    assert (dist_dir_path / "doc" / "mf6io.pdf").is_file()
-
-    if full:
-        # check other custom-built documentation
-        assert (dist_dir_path / "doc" / "release.pdf").is_file()
-        assert (dist_dir_path / "doc" / "mf5to6.pdf").is_file()
-        assert (dist_dir_path / "doc" / "zonebudget.pdf").is_file()
-        assert (dist_dir_path / "doc" / "mf6suptechinfo.pdf").is_file()
-        assert (dist_dir_path / "doc" / "mf6examples.pdf").is_file()
-
-        # check publications downloaded from USGS site
-        for pub in [
-            "tm6a55",
-            "tm6a56",
-            "tm6a57",
-            "tm6a61",
-            "tm6a62",
-        ]:
-            assert (dist_dir_path / "doc" / f"{pub}.pdf").is_file()
-
-
+"""
 def test_examples(dist_dir_path, full):
     if not full:
         pytest.skip(reason="examples not included in minimal distribution")
@@ -171,31 +105,10 @@ def test_examples(dist_dir_path, full):
             continue
         pprint(subprocess.check_output([str(script_path)], cwd=p).decode().split())
         break
+"""
 
 
 def test_binaries(dist_dir_path, approved):
     bin_path = dist_dir_path / "bin"
-    assert (bin_path / f"mf6{_eext}").is_file()
-    assert (bin_path / f"zbud6{_eext}").is_file()
-    assert (bin_path / f"mf5to6{_eext}").is_file()
-    assert (bin_path / f"libmf6{_soext}").is_file()
+    assert (bin_path / f"usg-transport{_eext}").is_file()
 
-    # get version string
-    output = " ".join(
-        subprocess.check_output([str(bin_path / f"mf6{_eext}"), "-v"]).decode().split()
-    ).lower()
-    assert output.startswith("mf6")
-
-    # make sure version string reflects approval
-    assert ("preliminary" in output) != approved
-
-    # check version numbers
-    version = output.lower().split(" ")[1]
-    print(version)
-    v_split = version.split(".")
-    assert len(v_split) >= 3
-    assert all(s.isdigit() for s in v_split[:3])
-    if approved:
-        assert len(v_split) == 3
-    else:
-        assert "dev" in v_split[3]
